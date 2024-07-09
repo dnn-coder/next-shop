03f46da1797ec7ac7196c0fb439c70fe7170f940
Danny Vivas <dvivasmera1.2@gmail.com>
2024-07-09 17:38:11 -0500

    first commit

--- src/app/(shop)/gender/[gender]/page.tsx
+++ src/app/(shop)/gender/[gender]/page.tsx
@@ -6,61 +6,52 @@ import { Pagination, ProductGrid, Title } from '@/components';
 import { Gender } from '@prisma/client';
 import { redirect } from 'next/navigation';
 
-
-
 interface Props {
   params: {
     gender: string;
-  },
+  };
   searchParams: {
-    page?: string; 
-  }
+    page?: string;
+  };
 }
 
-
 export default async function GenderByPage({ params, searchParams }: Props) {
-
   const { gender } = params;
 
-  const page = searchParams.page ? parseInt( searchParams.page ) : 1;
+  const page = searchParams.page ? parseInt(searchParams.page) : 1;
 
-  const { products, currentPage, totalPages } = await getPaginatedProductsWithImages({ 
-    page, 
-    gender: gender as Gender,
-  });
+  const { products, currentPage, totalPages } =
+    await getPaginatedProductsWithImages({
+      page,
+      gender: gender as Gender,
+    });
 
-
-  if ( products.length === 0 ) {
-    redirect(`/gender/${ gender }`);
+  if (products.length === 0) {
+    redirect(`/gender/${gender}`);
   }
-  
 
-  const labels: Record<string, string>  = {
-    'men': 'para hombres',
-    'women': 'para mujeres',
-    'kid': 'para niños',
-    'unisex': 'para todos'
-  }
+  const labels: Record<string, string> = {
+    men: 'hombre',
+    women: 'mujer',
+    kid: 'niños',
+    unisex: 'todos',
+  };
 
   // if ( id === 'kids' ) {
   //   notFound();
   // }
 
-
   return (
     <>
       <Title
-        title={`Artículos de ${ labels[gender] }`}
+        title={`Artículos para ${labels[gender]}`}
         subtitle="Todos los productos"
         className="mb-2"
       />
 
-      <ProductGrid 
-        products={ products }
-      />
+      <ProductGrid products={products} />
 
-      <Pagination totalPages={ totalPages }  />
-      
+      <Pagination totalPages={totalPages} />
     </>
   );
 }
